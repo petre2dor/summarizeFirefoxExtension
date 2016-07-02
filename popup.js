a38849da068ddfc0f574f7b4a68e993546372a7c
petre2dor <petre.tudor@protonmail.com>
2016-07-02 20:27:49 +0200

    remove useless comments, console.log and improve description

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/**
+ * this code loads only if the user clicks on the popup icon
+ */
+
 
 /**
  * Get the current URL.
@@ -9,42 +10,16 @@
  *   is found.
  */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+    chrome.tabs.query(queryInfo, function(tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        callback(url);
+    });
 }
 
 /**
@@ -54,28 +29,27 @@ function getCurrentTabUrl(callback) {
  *   The callback gets a string that describes the failure reason.
  */
 function getSummary(url, callback, errorCallback) {
-  var summarizeApiUrl = 'http://46.101.138.193:8000/summary';
-  var x = new XMLHttpRequest();
+    var summarizeApiUrl = 'http://46.101.138.193:8000/summary';
+    var x = new XMLHttpRequest();
 
-  x.open('GET', summarizeApiUrl + '?url=' + url);
-  // it responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from summaryApi.
-    var response = x.response;
-    console.log(response);
-    if (!response || !response) {
-      errorCallback('No response.');
-      return;
-    }
-    console.log(response.title);
-    console.log(response.body);
-    callback(response.title, response.body);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
+    x.open('GET', summarizeApiUrl + '?url=' + url);
+    // it responds with JSON, so let Chrome parse it.
+    x.responseType = 'json';
+    x.onload = function() {
+        // Parse and process the response from summaryApi.
+        var response = x.response;
+
+        if (!response || !response) {
+            errorCallback('No response.');
+            return;
+        }
+
+        callback(response.title, response.body);
+    };
+    x.onerror = function() {
+        errorCallback('Network error.');
+    };
+    x.send();
 }
 
 
@@ -85,18 +59,19 @@ function renderResults(title, summary) {
   document.getElementById('summary').textContent = summary;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
+document.addEventListener('DOMContentLoaded', function clickOnPopupIcon(event) {
+    document.removeEventListener(event, clickOnPopupIcon, false); // remove listener, no longer needed
+    getCurrentTabUrl(function(url) {
         // Put the image URL in Google search.
         renderResults('Getting summary for: ', url);
 
         getSummary( url,
-                    function(title, summary) {
-                        renderResults(title, summary);
-                    },
-                    function(errorMessage) {
-                        renderResults('Cannot display summary. ' + errorMessage);
-                    }
+            function(title, summary) {
+                renderResults(title, summary);
+            },
+            function(errorMessage) {
+                renderResults('Cannot display summary. ' + errorMessage);
+            }
         );
-  });
-});
+    });
+}, false);
